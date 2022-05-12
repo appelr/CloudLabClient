e552a82d891e9d6a72b1afaa65b8eaa051e91289
Randolf Appel <appelr@Randolfs-MacBook-Pro.local>
2022-05-12 17:41:51 +0200

    Changed output to table

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const { printTable } = require('console-table-printer');
 
 const data = {
     data: 'O Hi this a test Car River Deer Car Bear and'
@@ -6,9 +7,20 @@ const data = {
 
 axios.post('https://cloudlabserver.herokuapp.com/getWordLengthFrequency', data)
     .then((res) => {
-        console.log(data);
-        console.log(res.data);
+        // Get data from response
+        var dataArray = res.data.split(" ");
+
+        // Create a table
+        const resultTable = [];
+
+        // Populate the table with values
+        for (let i = 0; i < dataArray.length - 1; i++) {
+          resultTable.push({ wordlength: i + 1, wordcount: dataArray[i]})
+        }
+
+        // Print table
+        printTable(resultTable);
+
     }).catch((err) => {
         console.error(err);
     });
-    
